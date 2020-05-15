5c2b380e801afa47e162cc5ddca4b4f7e483be15
anton <ant.monakhov@gmail.com>
2020-05-15 23:23:09 +0300

    changes

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your telegram token from BotFather (str)
-TELEGRAM_TOKEN = ''
+TELEGRAM_TOKEN = '1141734539:AAHbNQpTt1Cu2hQMBzN3B88JH8j5VgYX4rA'
 # openweathermap appid (str)
-WEATHER_APPID = ''
+WEATHER_APPID = 'c1bf2c0152d50c866576aed3946641c9'
 # owner's chatID (int):
-OWNER = 00000000000000
+OWNER = 285554554
 # SOCKS5 Proxy server URL (str)
 PROXY = 'socks5://address:port'
