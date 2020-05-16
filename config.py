d9f97c5c9e8d53c6cd6f6758d990ba3bcc7a6801
admonakhov <65421105+admonakhov@users.noreply.github.com>
2020-05-16 18:03:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Your telegram token from BotFather (str)
-TELEGRAM_TOKEN = '1141734539:AAHbNQpTt1Cu2hQMBzN3B88JH8j5VgYX4rA'
+TELEGRAM_TOKEN = ''
 # owner's chatID (int):
-OWNER = 285554554
+OWNER = 
 # SOCKS5 Proxy server URL (str)
 PROXY = 'socks5://address:port'
